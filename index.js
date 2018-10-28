1984f9554f25f88c50de5561a74dd16ff677d239
Stepan Manko <stepan_manko@mckinsey.com>
2018-10-28 22:12:05 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: ['POST'],
+    path: '/',
+    handler: function (request, h) {
+	return request.payload;
+    }
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
